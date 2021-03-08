6774b85752f2c046caaf83091037ff438912700f
jerinjames999 <jerinjameskarippa999@gmail.com>
2021-03-08 12:48:59 +0530

    server and integrating socketio

--- src/index.js
+++ src/index.js
@@ -1,33 +1,46 @@
-const express = require('express');
-const http = require('http');
 const path = require('path')
-const socketio = require('socket.io');
+const http = require('http')
+const express = require('express')
+const socketio = require('socket.io')
+const Filter = require('bad-words')
+const {generateMessage,generateLocationMessage} = require('./utils/messages')
 
-const app = express();
-const server = http.createServer(app);
-const io = socketio(server);
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
 
-const port = process.env.PORT || 3000;
-const publicDirectoryPath = path.join(__dirname,'../public');
+const port = process.env.PORT || 3000
+const publicDirectoryPath = path.join(__dirname, '../public')
 
-app.use(express.static(publicDirectoryPath));
+app.use(express.static(publicDirectoryPath))
 
-let count = 0;
-io.on('connection',(socket) => {
-    console.log('New websocket connection')
-    socket.emit('countUpdated', count)
+io.on('connection', (socket) => {
+    console.log('New WebSocket connection')
 
-    socket.on('increment',() => {
-        count++;
-        //socket.emit('countUpdated',count)
-        io.emit('countUpdated',count)
+    socket.emit('message', generateMessage('Welcome!!'))
+    socket.broadcast.emit('message', generateMessage('A new user has joined!'))
+
+    socket.on('sendMessage', (message, callback) => {
+        const filter = new Filter()
+
+        if (filter.isProfane(message)) {
+            return callback('Profanity is not allowed!')
+        }
+
+        io.emit('message', generateMessage(message))
+        callback()
+    })
+
+    socket.on('sendLocation', (coords, callback) => {
+        io.emit('locationMessage', generateLocationMessage(`https://google.com/maps?q=${coords.latitude},${coords.longitude}`))
+        callback()
+    })
+
+    socket.on('disconnect', () => {
+        io.emit('message', generateMessage('A user has left!'))
     })
 })
 
-server.listen(port,() => {
-    console.log(`Server is up on port ${port}!`);
+server.listen(port, () => {
+    console.log(`Server is up on port ${port}!`)
 })
-
-// Web sockets allow full duplex communication. ie client can initate the communication with server and vice versa.
-// WebSocket is a separete protocol from HTTP.
-// Persistent communication between client and server.
